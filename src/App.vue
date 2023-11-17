b536a8bafda41f29bcf5dd0378cb82e16d700bb2
filippo <riccardo.gabba02@gmail.com>
2023-11-17 16:47:44 +0100

    initialized the header + linked the header to the main app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+<Header/>
+
+</template>
+
+<script>
+import Header from './components/Header.vue'
+  export default {
+    components:{
+      Header,
+    },
+    data(){
+      return{
+       
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container d-flex justify-content-between">
+    <div>
+      <img src="../images/dc-logo.png" alt="">
+    </div>
+    <div class="d-flex align-items-center">
+      <h5 class="p-2">characters</h5>
+      <h5 class="p-2">comics</h5>
+      <h5 class="p-2"> movies</h5>
+      <h5 class="p-2">tv</h5>
+      <h5 class="p-2">games</h5>
+      <h5 class="p-2"> collectbles</h5>
+      <h5 class="p-2">videos</h5>
+      <h5 class="p-2">fans</h5>
+      <h5 class="p-2">news</h5>
+      <h5 class="p-2">shop</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
